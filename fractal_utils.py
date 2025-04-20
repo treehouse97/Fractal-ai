f335a166cc124c65f38c90f8471ecc761dc978ad
treehouse97 <wariest-pavlova2c@icloud.com>
2025-04-20 07:40:52 -0400

    Update fractal_utils.py

--- fractal_utils.py
+++ fractal_utils.py
@@ -11,9 +11,11 @@ def draw_fractal(ax, x, y, angle, depth, length):
     draw_fractal(ax, x_end, y_end, angle + np.pi/6, depth - 1, length * 0.7)
 
 def generate_fractal_image(depth=5):
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax.set_facecolor('black')
-    ax.axis('off')
-    draw_fractal(ax, 0, 0, np.pi/2, depth, 100)
-    fig.tight_layout()
+    fig, ax = plt.subplots(figsize=(4, 6), dpi=100)
+    ax.set_facecolor("black")
+    ax.axis("off")
+    ax.set_xlim(-150, 150)  # set fixed limits to avoid rendering nothing
+    ax.set_ylim(0, 300)
+    draw_fractal(ax, 0, 0, np.pi / 2, depth, 100)
+    fig.tight_layout(pad=0)
     return fig
